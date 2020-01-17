65cbf3f4e06c330f1c950c2ae29c12f5eb66de80
Mathias Müller <mathias.mueller@uzh.ch>
2020-01-17 10:18:11 +0100

    fir preprocess base

--- scripts/preprocessing/preprocess_all.sh
+++ scripts/preprocessing/preprocess_all.sh
@@ -5,7 +5,7 @@ if [[ $1 == "mode=sbatch" ]]; then
   base=/net/cephfs/home/mathmu/scratch/noise-distill
 else
   script_dir=`dirname "$0"`
-  base=$script_dir/..
+  base=$script_dir/../..
 fi;
 
 for noise_type in misaligned_sent, misordered_words_src, misordered_words_trg, wrong_lang_fr_src, wrong_lang_fr_trg, untranslated_en_src, untranslated_de_trg, short_max2, short_max5, raw_paracrawl; do
