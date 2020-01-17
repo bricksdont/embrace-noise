ae5e2675bd0c1075c5b92639f1a7204dcacd23f7
Mathias Müller <mathias.mueller@uzh.ch>
2020-01-17 16:18:10 +0100

    parallelize preprocess

--- scripts/preprocessing/preprocess_all.sh
+++ scripts/preprocessing/preprocess_all.sh
@@ -76,7 +76,7 @@ for noise_type in misaligned_sent misordered_words_src misordered_words_trg wron
       shared_models_sub=$shared_models/$noise_type.$noise_amount
       mkdir -p $shared_models_sub
 
-      sbatch --cpus-per-task=8 --time=12:00:00 --mem=64G --partition=hydra $base/scripts/preprocessing/preprocess_generic.sh $data_sub $shared_models_sub $bpe_vocab_threshold $bpe_total_symbols
+      sbatch --cpus-per-task=4 --time=12:00:00 --mem=64G --partition=hydra $base/scripts/preprocessing/preprocess_generic.sh $data_sub $shared_models_sub $bpe_vocab_threshold $bpe_total_symbols
     done
 done
 
@@ -98,4 +98,4 @@ done
 shared_models_sub=$shared_models/baseline
 mkdir -p $shared_models_sub
 
-sbatch --cpus-per-task=8 --time=12:00:00 --mem=64G --partition=hydra $base/scripts/preprocessing/preprocess_generic.sh $data_sub $shared_models_sub $bpe_vocab_threshold $bpe_total_symbols
+sbatch --cpus-per-task=4 --time=12:00:00 --mem=64G --partition=hydra $base/scripts/preprocessing/preprocess_generic.sh $data_sub $shared_models_sub $bpe_vocab_threshold $bpe_total_symbols
