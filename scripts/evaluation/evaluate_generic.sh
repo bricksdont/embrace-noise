e0ad8a0ea478bba1c3bf806d13db452ebcd85dd5
Mathias Müller <mathias.mueller@uzh.ch>
2020-01-19 20:04:43 +0100

    debug eval

--- scripts/evaluation/evaluate_generic.sh
+++ scripts/evaluation/evaluate_generic.sh
@@ -2,6 +2,7 @@
 
 # calling script needs to set
 
+# $data
 # $data_sub
 # $translations_sub
 # $evaluations_sub
@@ -12,7 +13,7 @@ for corpus in dev test; do
 
     # compute case-sensitive BLEU on detokenized data
 
-    cat $translations_sub/$corpus.$trg | sacrebleu $data_sub/$corpus.$trg > $evaluations_sub/$corpus.bleu
+    cat $translations_sub/$corpus.$trg | sacrebleu $data/raw/$corpus.$trg > $evaluations_sub/$corpus.bleu
 
     echo "$evaluations_sub/$corpus.bleu"
     cat $evaluations_sub/$corpus.bleu
