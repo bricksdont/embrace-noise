19a01dc17d3e2145f3a2600a6aa7fea4e58088ba
Mathias Müller <mathias.mueller@uzh.ch>
2020-01-19 19:12:19 +0100

    debug eval

--- scripts/evaluation/evaluate_all.sh
+++ scripts/evaluation/evaluate_all.sh
@@ -26,6 +26,10 @@ mkdir -p $evaluations_sub
 
 . $base/scripts/evaluation/evaluate_generic.sh
 
+# try for baseline only
+
+exit
+
 for noise_type in misaligned_sent misordered_words_src misordered_words_trg wrong_lang_fr_src wrong_lang_fr_trg untranslated_en_src untranslated_de_trg short_max2 short_max5 raw_paracrawl; do
   for noise_amount in 05 10 20 50 100; do
 
