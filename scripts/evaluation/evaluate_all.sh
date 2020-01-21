0705bebbb22bdb0d26072dc90e4f356670a1cb29
Mathias Müller <mathias.mueller@uzh.ch>
2020-01-21 21:27:04 +0100

    debug eval all

--- scripts/evaluation/evaluate_all.sh
+++ scripts/evaluation/evaluate_all.sh
@@ -23,10 +23,6 @@ mkdir -p $evaluations_sub
 
 . $base/scripts/evaluation/evaluate_generic.sh
 
-# try for baseline only
-
-exit
-
 for noise_type in misaligned_sent misordered_words_src misordered_words_trg wrong_lang_fr_src wrong_lang_fr_trg untranslated_en_src untranslated_de_trg short_max2 short_max5 raw_paracrawl; do
   for noise_amount in 05 10 20 50 100; do
 
@@ -36,12 +32,6 @@ for noise_type in misaligned_sent misordered_words_src misordered_words_trg wron
     data_sub=$data/$noise_type.$noise_amount
     translations_sub=$translations/$noise_type.$noise_amount
     evaluations_sub=$evaluations/$noise_type.$noise_amount
-
-    if [[ -d $evaluations_sub ]]; then
-        echo "Folder exists: $evaluations_sub"
-        echo "Skipping."
-        continue
-    fi
 
     mkdir -p $evaluations_sub
 
