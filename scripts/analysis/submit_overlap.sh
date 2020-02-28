1feb6787656acc2c95136e09cfdec9f213b55540
Mathias Müller <mathias.mueller@uzh.ch>
2020-02-28 10:21:15 +0100

    debug overlap

--- scripts/analysis/submit_overlap.sh
+++ scripts/analysis/submit_overlap.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sbatch --cpus-per-task=1 --time=00:15:00 --mem=16G --partition=hydra /net/cephfs/home/mathmu/scratch/noise-distill/scripts/analysis/overlap.sh
+sbatch --cpus-per-task=1 --time=00:15:00 --mem=32G --partition=hydra /net/cephfs/home/mathmu/scratch/noise-distill/scripts/analysis/overlap.sh
