0bc5d8533af899e89d60231bd2df20db37be287c
Mathias Müller <mathias.mueller@uzh.ch>
2020-03-03 15:55:41 +0100

    debug overlap

--- scripts/analysis/assemble_analysis_data.sh
+++ scripts/analysis/assemble_analysis_data.sh
@@ -8,6 +8,8 @@ filtered=$base/filtered
 mined=$base/mined
 scripts=$base/scripts
 
+MOSES=$base/tools/moses-scripts/scripts
+
 mkdir -p $analysis
 
 src=de
@@ -21,13 +23,26 @@ offset=3520019
 
 for method in adq adq-dom; do
     for fraction in 0.25 0.5 0.75; do
-        paste $data/raw_paracrawl.100.dcce.$method.$fraction/train.bpe.$src $data/raw_paracrawl.100.dcce.$method.$fraction/train.bpe.$trg > $analysis/dcce.$method.$fraction.all
+        paste $data/raw_paracrawl.100.dcce.$method.$fraction/train.bpe.$src $data/raw_paracrawl.100.dcce.$method.$fraction/train.bpe.$trg > $analysis/dcce.$method.$fraction.bpe.all
 
-        sed -n "$offset,$ p" $analysis/dcce.$method.$fraction.all > $analysis/dcce.$method.$fraction
+        sed -n "$offset,$ p" $analysis/dcce.$method.$fraction.bpe.all > $analysis/dcce.$method.$fraction.bpe
+
+        # postprocess DCCE to compare against
+
+        cut -f2 $analysis/dcce.$method.$fraction.bpe > $analysis/dcce.$method.$fraction.bpe.$src
+        cut -f3 $analysis/dcce.$method.$fraction.bpe > $analysis/dcce.$method.$fraction.bpe.$trg
+
+        for lang in $src $trg; do
+            cat $analysis/dcce.$method.$fraction.bpe.$lang | sed -r 's/@@( |$)//g' > $analysis/dcce.$method.$fraction.tok.$lang
+
+            cat $analysis/dcce.$method.$fraction.tok.$lang | $MOSES/tokenizer/detokenizer.perl -l $lang > $analysis/dcce.$method.$fraction.$lang
+        done
+
+        paste $analysis/dcce.$method.$fraction.$src $analysis/dcce.$method.$fraction.$trg > $analysis/dcce.$method.$fraction
     done
 done
 
-# raw without preprocessing to compare against filtered
+# raw without preprocessing to compare against filtered and DCCE
 
 analysis_sub=$analysis/mined_raw
 
@@ -43,20 +58,6 @@ for method in mine score; do
 
     for fraction in 0.25 0.5 0.75; do
         cat $analysis_sub/mined.$method.100 | python $scripts/preprocessing/head_fraction.py --fraction $fraction --size $num_lines > $analysis_sub/mined.$method.$fraction
-    done
-done
-
-# with preprocessing to compare against DCCE
-
-analysis_sub=$analysis/mined_bpe
-
-mkdir -p $analysis_sub
-
-for method in mine score; do
-    for fraction in 0.25 0.5 0.75; do
-      paste $data/raw_paracrawl.100.mined.$method.$fraction/train.bpe.$src $data/raw_paracrawl.100.mined.$method.$fraction/train.bpe.$trg > $analysis_sub/mined.$method.$fraction.all
-
-      sed -n "$offset,$ p" $analysis_sub/mined.$method.$fraction.all > $analysis_sub/mined.$method.$fraction
     done
 done
 
