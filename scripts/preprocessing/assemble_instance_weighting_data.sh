eb093be2817595561bd756ccbe85a490cfffe0c6
Mathias Müller <mathias.mueller@uzh.ch>
2020-03-19 18:22:24 +0100

    assemble instance weighting data_subs

--- scripts/preprocessing/assemble_instance_weighting_data.sh
+++ scripts/preprocessing/assemble_instance_weighting_data.sh
@@ -10,6 +10,7 @@ trg=en
 scripts=$base/scripts
 
 data=$base/data
+preprocessed=$base/preprocessed
 filtered=$base/filtered
 
 mined=$base/mined
@@ -25,75 +26,76 @@ shopt -s nullglob
 
 for mined_sub in $mined/*; do
 
-  mining_method=score
+    mining_method=score
 
-          model_name=$(basename $mined_sub)
-          original_name=$model_name
+    model_name=$(basename $mined_sub)
+    original_name=$model_name
 
-          model_name=$model_name.mined.$mining_method.instance_weighting
-          data_sub=$data/$model_name
+    model_name=$model_name.mined.$mining_method.instance_weighting
+    data_sub=$data/$model_name
 
-          if [[ -d $data_sub ]]; then
-            echo "data_sub exists: $data_sub"
-            echo "Skipping."
-            continue
-          fi
+    if [[ -d $data_sub ]]; then
+      echo "data_sub exists: $data_sub"
+      echo "Skipping."
+      continue
+    fi
 
-          num_lines_baseline=`cat $filtered/$original_name/train.bpe.$src | wc -l`
+    num_lines_baseline=`cat $data/baseline/train.bpe.$src | wc -l`
 
-          origin_sub=$(mktemp -d)
+    origin_sub=$(mktemp -d)
 
-          cat $mined_sub/mined.$mining_method.sorted | cut -f2 > $origin_sub/train.$src
-          cat $mined_sub/mined.$mining_method.sorted | cut -f3 > $origin_sub/train.$trg
+    cat $mined_sub/mined.$mining_method.sorted | cut -f2 > $origin_sub/train.$src
+    cat $mined_sub/mined.$mining_method.sorted | cut -f3 > $origin_sub/train.$trg
 
-          python $scripts/preprocessing/create_weights.py --method ones --size $num_lines_baseline > $origin_sub/scores.1
+    python $scripts/preprocessing/create_weights.py --method ones --size $num_lines_baseline > $origin_sub/scores.1
 
-          cut -f1 $mined_sub/mined.$mining_method.sorted > $origin_sub/scores.2
+    cut -f1 $mined_sub/mined.$mining_method.sorted > $origin_sub/scores.2
 
-          cat $origin_sub/scores.1 $origin_sub/scores.2 > $origin_sub/train.weights
+    cat $origin_sub/scores.1 $origin_sub/scores.2 > $origin_sub/train.weights
 
-          for lang in $src $trg; do
-            cat $origin_sub/train.$lang | perl $MOSES/tokenizer/normalize-punctuation.perl $lang > $origin_sub/train.normalized.$lang
-            cat $origin_sub/train.normalized.$lang | perl $MOSES/tokenizer/tokenizer.perl -a -q -l $lang > $origin_sub/train.tok.$lang
+    for lang in $src $trg; do
+      cat $origin_sub/train.$lang | perl $MOSES/tokenizer/normalize-punctuation.perl $lang > $origin_sub/train.normalized.$lang
+      cat $origin_sub/train.normalized.$lang | perl $MOSES/tokenizer/tokenizer.perl -a -q -l $lang > $origin_sub/train.tok.$lang
 
-            subword-nmt apply-bpe -c $shared_models/baseline/$src$trg.bpe \
-            --vocabulary $shared_models/baseline/vocab.$lang \
-            --vocabulary-threshold $bpe_vocab_threshold < $origin_sub/train.tok.$lang > $origin_sub/train.bpe.$lang
-          done
+      subword-nmt apply-bpe -c $shared_models/baseline/$src$trg.bpe \
+      --vocabulary $shared_models/baseline/vocab.$lang \
+      --vocabulary-threshold $bpe_vocab_threshold < $origin_sub/train.tok.$lang > $origin_sub/train.bpe.$lang
+    done
 
-        . $scripts/preprocessing/concat_with_baseline_generic.sh
+    . $scripts/preprocessing/concat_with_baseline_generic.sh
 
 done
 
 for dcce_sub in $dcce/*; do
 
-      for dcce_method in adq adq-dom; do
+  for dcce_method in adq adq-dom; do
 
-          model_name=$(basename $dcce_sub)
+      model_name=$(basename $dcce_sub)
+      original_name=$model_name
 
-          model_name=$model_name.dcce.$dcce_method.instance_weighting
-          data_sub=$data/$model_name
+      model_name=$model_name.dcce.$dcce_method.instance_weighting
+      data_sub=$data/$model_name
 
-          if [[ -d $data_sub ]]; then
-            echo "data_sub exists: $data_sub"
-            echo "Skipping."
-            continue
-          fi
+      if [[ -d $data_sub ]]; then
+        echo "data_sub exists: $data_sub"
+        echo "Skipping."
+        continue
+      fi
 
-          num_lines_baseline=`cat $dcce_sub/scores.$dcce_method.all.sorted | wc -l`
+      num_lines_baseline=`cat $data/baseline/train.bpe.$src | wc -l`
 
-          origin_sub=$(mktemp -d)
+      origin_sub=$(mktemp -d)
 
-          cat $dcce_sub/scores.$dcce_method.all.sorted | cut -f2 > $origin_sub/train.bpe.$src
-          cat $dcce_sub/scores.$dcce_method.all.sorted | cut -f3 > $origin_sub/train.bpe.$trg
+      cat $dcce_sub/scores.$dcce_method.all.sorted | cut -f2 > $origin_sub/train.bpe.$src
+      cat $dcce_sub/scores.$dcce_method.all.sorted | cut -f3 > $origin_sub/train.bpe.$trg
 
-          python $scripts/preprocessing/create_weights.py --method ones --size $num_lines_baseline > $origin_sub/scores.1
+      python $scripts/preprocessing/create_weights.py --method ones --size $num_lines_baseline > $origin_sub/scores.1
 
-          cut -f1 $dcce_sub/scores.$dcce_method.all.sorted > $origin_sub/scores.2
+      cut -f1 $dcce_sub/scores.$dcce_method.all.sorted > $origin_sub/scores.2
 
-          cat $origin_sub/scores.1 $origin_sub/scores.2 > $origin_sub/train.weights
+      cat $origin_sub/scores.1 $origin_sub/scores.2 > $origin_sub/train.weights
 
-          . $scripts/preprocessing/concat_with_baseline_generic.sh
+      . $scripts/preprocessing/concat_with_baseline_generic.sh
 
   done
 done
