e68a3c7118bac0d1e2aeabd096b05333eae2f3b0
Mathias Müller <mathias.mueller@uzh.ch>
2020-04-08 10:28:26 +0200

    FA weights from params

--- scripts/fast_align/weights_from_params.sh
+++ scripts/fast_align/weights_from_params.sh
@@ -13,11 +13,13 @@ trg=en
 data=$base/data
 
 fast_align=$base/fast_align
+alignments=$base/alignments
 
 for model_name in raw_paracrawl.100.filtered; do
 
    data_sub=$data/$model_name
    fast_align_sub=$fast_align/$model_name
+   alignments_sub=$alignments/$model_name
 
    python $base/scripts/fast_align/weights_from_params.py \
        --params $fast_align_sub/params.out \
