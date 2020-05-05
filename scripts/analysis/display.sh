1c894848d238ee62f5c46a011fae0ad6b378cc07
Mathias Müller <mathias.mueller@uzh.ch>
2020-05-05 22:48:10 +0200

    word-level FA models, strided sample

--- scripts/analysis/display.sh
+++ scripts/analysis/display.sh
@@ -20,3 +20,18 @@ python3 display.py --source train.bpe.sample.de --target train.bpe.sample.en \
                     all.ignore all.only all.mean all.min all.max \
                     filtered.ignore filtered.only filtered.mean filtered.min filtered.max > \
     results.all.csv
+
+python3 display.py --source train.bpe.sample.de --target train.bpe.sample.en \
+    --weights raw_paracrawl.100.filtered.baseline.ignore/sample \
+              raw_paracrawl.100.filtered.baseline.only/sample \
+              raw_paracrawl.100.filtered.baseline.mean/sample \
+              raw_paracrawl.100.filtered.baseline.min/sample \
+              raw_paracrawl.100.filtered.baseline.max/sample \
+              raw_paracrawl.100.filtered.baseline.word_level.ignore/sample \
+              raw_paracrawl.100.filtered.baseline.word_level.only/sample \
+              raw_paracrawl.100.filtered.baseline.word_level.mean/sample \
+              raw_paracrawl.100.filtered.baseline.word_level.min/sample \
+              raw_paracrawl.100.filtered.baseline.word_level.max/sample \
+    --weights-names baseline.ignore baseline.only baseline.mean baseline.min baseline.max \
+                    baseline.word_level.ignore baseline.word_level.only baseline.word_level.mean baseline.word_level.min baseline.word_level.max > \
+    results.baseline.subword_vs_word_level.csv
