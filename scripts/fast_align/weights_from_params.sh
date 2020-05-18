afaf555e987032122ddf51db134c05e1fd7d6948
Mathias Müller <mathias.mueller@uzh.ch>
2020-05-18 16:53:11 +0200

    improve FA token weights algorithm

--- scripts/fast_align/weights_from_params.sh
+++ scripts/fast_align/weights_from_params.sh
@@ -27,38 +27,38 @@ for model_name in raw_paracrawl.100.filtered; do
         cat $data_sub/train.bpe.$lang | python $base/scripts/analysis/strided_sample.py > $data_sub/train.bpe.sample.$lang
     done
 
-    for fast_align_model in baseline raw_paracrawl.100 raw_paracrawl.100.filtered; do
-
-         echo "fast_align_model: $fast_align_model"
-
-         for reverse_method in min max mean geomean ignore only; do
-
-             echo "reverse_method: $reverse_method"
-
-             fast_align_sub=$fast_align/$fast_align_model
-             fast_align_sub_reverse="$fast_align/$fast_align_model"_reverse
-             alignments_sub=$alignments/$model_name.$fast_align_model.$reverse_method
-
-             if [[ -d $alignments_sub ]]; then
-              echo "alignments_sub exists: $alignments_sub"
-              echo "Skipping."
-              continue
-             fi
-
-             mkdir -p $alignments_sub
-
-             word_level_arg=""
-             smooth_method_arg=""
-
-             sbatch --cpus-per-task=1 --time=12:00:00 --mem=64G --partition=hpc $base/scripts/fast_align/weights_from_params_generic.sh \
-                 $base $fast_align_sub $alignments_sub $data_sub $fast_align_sub_reverse $reverse_method $word_level_arg $smooth_method_arg
-
-          done
-    done
+#    for fast_align_model in baseline raw_paracrawl.100 raw_paracrawl.100.filtered; do
+#
+#         echo "fast_align_model: $fast_align_model"
+#
+#         for reverse_method in min max mean geomean ignore only; do
+#
+#             echo "reverse_method: $reverse_method"
+#
+#             fast_align_sub=$fast_align/$fast_align_model
+#             fast_align_sub_reverse="$fast_align/$fast_align_model"_reverse
+#             alignments_sub=$alignments/$model_name.$fast_align_model.$reverse_method
+#
+#             if [[ -d $alignments_sub ]]; then
+#              echo "alignments_sub exists: $alignments_sub"
+#              echo "Skipping."
+#              continue
+#             fi
+#
+#             mkdir -p $alignments_sub
+#
+#             word_level_arg=""
+#             smooth_method_arg=""
+#
+#             sbatch --cpus-per-task=1 --time=12:00:00 --mem=64G --partition=hpc $base/scripts/fast_align/weights_from_params_generic.sh \
+#                 $base $fast_align_sub $alignments_sub $data_sub $fast_align_sub_reverse $reverse_method $word_level_arg $smooth_method_arg
+#
+#          done
+#    done
 
     # word-level models
 
-    for fast_align_model in baseline.word_level raw_paracrawl.100.word_level raw_paracrawl.100.filtered.word_level; do
+    for fast_align_model in raw_paracrawl.100.filtered.word_level; do # baseline.word_level raw_paracrawl.100.word_level
 
          echo "fast_align_model: $fast_align_model"
 
